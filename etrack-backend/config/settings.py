913d97ab22ac1ecb99e9ce6d183187d90797d3aa
NilaRamamoorthy <nilasibiraj@gmail.com>
2026-03-03 18:28:01 +0530

    Connected

--- etrack-backend/config/settings.py
+++ etrack-backend/config/settings.py
@@ -55,7 +55,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
+
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://train-tracking-app.vercel.app",
+]
+
+from corsheaders.defaults import default_headers
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "authorization",
+]
 
 ROOT_URLCONF = 'config.urls'
 
